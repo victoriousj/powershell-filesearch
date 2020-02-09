67856ac38909cf6f89350690e969c850e1e034b0
Victor Johnson <victordjohnson@gmail.com>
2020-02-08 22:28:05 -0600

    simplify logic

--- FileSearch.ps1
+++ FileSearch.ps1
@@ -1,47 +1,24 @@
 $fileSearch = $true
 
-for ( $i = 0; $i -lt $args.count; $i++ ) {
-    if ($args[$i] -match '-dr') 
-    {
-        $fileSearch = $false
-    }
+if ($args.IndexOf('-dr') -gt -1) {
+    $fileSearch = $false
 } 
 
-if ($fileSearch) 
-{
-    $param = '*'+($args -join "*")+'*'
+for ( $i = 0; $i -lt $args.count; $i++ ) {
+    if ($args[$i] -notmatch '-dr') 
+    {
+        $param += '*'+$args[$i]+'*'
+    }
+}
 
-    Write-Host 'searching for: '($args -join ' ')'...' -Separator ""
-    $fileName = get-childitem -recurse -File -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName -First 1
-    if ($fileName) 
-    {
-        Write-Host $fileName
-        start-process -filepath $fileName
-    }
-    else 
-    {
-        Write-Host 'file not found'
-    }
+Write-Host 'searching for: "'($param.Replace('*', " ").Trim())'"...' -Separator ""
+$fileName = get-childitem -recurse -File:($fileSearch) -Directory:(!$fileSearch) -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName -First 1
+if ($fileName) 
+{
+    Write-Host $fileName
+    start-process -filepath $fileName
 }
 else 
 {
-    $param = ""
-    for ( $i = 0; $i -lt $args.count; $i++ ) {
-        if ($args[$i] -notmatch '-dr') 
-        {
-            $param += '*'+$args[$i]+'*'
-        }
-    }
-
-    Write-Host 'searching for directory: '$param.Replace("*", "")'...' -Separator ""
-    $fileName = get-childitem -recurse -Directory -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName -First 1
-    if ($fileName) 
-    {
-        Write-Host $fileName
-        start-process -filepath $fileName
-    }
-    else 
-    {
-        Write-Host 'folder not found'
-    }
+    Write-Host 'nothing was found'
 }
