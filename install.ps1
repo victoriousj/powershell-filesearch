82ac905b00e9c89e1f6849e8b21fb122f4bdd9b9
vic <vjohnson@ImageTrend.com>
2020-02-10 13:34:50 -0600

    added a check on the PATH to make sure it was not already installed

--- install.ps1
+++ install.ps1
@@ -1,7 +1,14 @@
 $PathLocation = (Get-Item ENV:LOCALAPPDATA | Select-Object -ExpandProperty  value) + '\Programs\'
 
-Copy-Item 'FileSearch' -Destination $PathLocation -Recurse
+Copy-Item 'FileSearch' -Destination $PathLocation -Recurse -Force
 
-[Environment]::SetEnvironmentVariable("Path", $env:Path + (";" + $PathLocation + "FileSearch"), "Machine")
+$FileSearchLocation = $PathLocation + 'FileSearch'
+
+$regexAddPath = [regex]::Escape($FileSearchLocation)
+$arrPath = $env:Path -split ';' | Where-Object {$_ -and $_ -notMatch "^$regexAddPath\\?"}
+
+$newPath =  ($arrPath + $FileSearchLocation) -join ';'
+
+[Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
 
 Write-Host "Installed FileSearch. Close this instance of PowerShell and open another in order to start using FileSearch"
