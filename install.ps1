cdcf4887242177b8fd3c9e1976c30851c42683f5
vic <vjohnson@ImageTrend.com>
2020-02-10 14:06:14 -0600

    modified the install file so that it only affects the current user

--- install.ps1
+++ install.ps1
@@ -1,14 +1,15 @@
 $PathLocation = (Get-Item ENV:LOCALAPPDATA | Select-Object -ExpandProperty  value) + '\Programs\'
 
-Copy-Item 'FileSearch' -Destination $PathLocation -Recurse -Force
-
 $FileSearchLocation = $PathLocation + 'FileSearch'
 
 $regexAddPath = [regex]::Escape($FileSearchLocation)
-$arrPath = $env:Path -split ';' | Where-Object {$_ -and $_ -notMatch "^$regexAddPath\\?"}
+
+$arrPath = ([Environment]::GetEnvironmentVariable("PATH", "User")) -split ';' | Where-Object {$_ -and $_ -notMatch "^$regexAddPath\\?"}
 
 $newPath =  ($arrPath + $FileSearchLocation) -join ';'
 
-[Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+Copy-Item 'FileSearch' -Destination $PathLocation -Recurse -Force
+
+[Environment]::SetEnvironmentVariable("Path", $newPath, "User")
 
 Write-Host "Installed FileSearch. Close this instance of PowerShell and open another in order to start using FileSearch"
