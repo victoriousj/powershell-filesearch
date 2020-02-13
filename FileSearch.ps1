40ef3ef80c7b26f7eb88964ae709b5d1db93c069
Victor Johnson <victordjohnson@gmail.com>
2020-02-12 18:59:09 -0600

    added multi-result display and selection

--- FileSearch.ps1
+++ FileSearch.ps1
@@ -1,12 +1,14 @@
-﻿if ($args.Length -eq 0) {return}
+﻿if ($args.Length -eq 0) {return}  
 
 $fileSearch = $true
-if ($args.IndexOf('-dr') -gt -1) {
+if ($args.IndexOf('-dr') -gt -1) 
+{
     $fileSearch = $false
-} 
+}
 
-foreach ($arg in $args) {
-    if ($arg -notmatch '-dr') 
+foreach ($arg in $args) 
+{
+    if ($arg -notmatch '-dr')     
     {
         $param += "*$arg*"
     }
@@ -14,15 +16,42 @@ foreach ($arg in $args) {
 
 $type = @({directory},{file})[$fileSearch]
 
-Write-Host "searching for $type - ""$($param.Replace('*', " ").Replace("  ", " ").Trim())"""
+Write-Host "`nsearching for $type - ""$($param.Replace('*', " ").Replace("  ", " ").Trim())""`n"
 
-$fileName = Get-ChildItem -Recurse -File:($fileSearch) -Directory:(!$fileSearch) -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName -First 1
-if ($fileName) 
+$results = @(Get-ChildItem -Recurse -File:($fileSearch) -Directory:(!$fileSearch) -ErrorAction SilentlyContinue -Filter $param | Select-Object -ExpandProperty FullName)
+
+if ($results.Length)
 {
-    Write-Host $fileName
-    start-process -filepath $fileName
+    $file = $results[0]
+    if ($results.Length -gt 1) 
+    {
+        Write-Host "Select one of the results:"
+        for ($index=0; $index -lt $results.Length; $index++)
+        { 
+            Write-Host "[$($index+1)] - $($results[$index])"
+        }
+        
+        Write-Host "[x] - exit`n"
+        do
+        {
+            $input = Read-Host "Which $($type)??"
+            if ($input -eq "x") {return}
+        }
+        until ($input -match '^[0-9]+$' -and [int]$input -lt $results.Length+1)
+
+        $file = $results[$input-1]
+    }
+
+    Write-Host $file
+    if ($results.Length -eq 1)
+    {
+        $input = Read-Host "`nopen this $($type)?? [Y/n]"
+        if ($input -eq "n") { return }
+    }
+
+    Start-Process -filepath "$file"
 }
-else 
+else
 {
     Write-Host "the $type was not found"
 }
